f3a80db47808d98d422e005c8b508352fe037a0d
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2023-11-05 23:19:34 +0100

    Welcome the user to this program / game

--- ArenaFighter/Program.cs
+++ ArenaFighter/Program.cs
@@ -2,11 +2,27 @@
 
 namespace ArenaFighter
 {
+	/// <summary>
+	/// The start class for this program / game
+	/// </summary>
 	internal class Program
 	{
+		/// <summary>
+		/// The name of this program
+		/// </summary>
+		public const string ProgramName = "The Arena fighter 2000";
+		
+		/// <summary>
+		/// The start point of this program / game
+		/// </summary>
+		/// <param name="args">Anything</param>
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// Welcome the user to this program / game
+			Console.WriteLine($"Welcome to {ProgramName}!");
+
+			// Make a pause in this program
+			Console.ReadKey();
 		}
 	}
 }
